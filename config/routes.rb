5e2da0fdfb3fcb96367132dc715e5c7b364bbe0a
Pablo Andrizzi <pandrizzi45@gmail.com>
2020-06-13 13:47:59 -0300

    done but update is not working. needs refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new'
+  get 'tasks', to: 'tasks#index'
+  get 'tasks/:id/edit', to: 'tasks#edit'
+  get 'tasks/:id', to: 'tasks#show'
+  patch 'tasks/:id', to: 'tasks#update'
+  delete 'tasks/:id', to: 'tasks#destroy'
 end
+
